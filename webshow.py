56fe8bb768fa108810aeb4416290a8595452853e
zhenyuw16 <33314453+zhenyuw16@users.noreply.github.com>
2019-10-24 19:50:01 +0800

    website display

---
+++ webshow.py
@@ -0,0 +1,37 @@
+import os
+from selenium import webdriver
+import re
+import time
+
+driver=webdriver.Chrome()
+driver.get(os.path.join(os.getcwd(),'step6.html'))
+
+stepArr = 'DDrdFFBDRRDDrFFdFFuBBLLUUDRRU'  #给定的还原序列
+
+shuffleArr = stepArr[::-1]
+shuffleArr = list(shuffleArr)
+for i in range(len(shuffleArr)):
+    ss = shuffleArr[i]
+    if ss >='A' and ss <='Z':
+        shuffleArr[i] = ss.lower()
+    else:
+        shuffleArr[i] = ss.upper()
+
+shuffleArr = str(shuffleArr)
+shuffleArr = re.sub('\'','',shuffleArr)
+shuffleArr = re.sub('\"','',shuffleArr)
+
+print('web opening done')
+time.sleep(3)
+
+driver.execute_script('randomRotate(' + shuffleArr + ');')
+print('shuffle done')
+time.sleep(3)
+
+driver.execute_script( 'totalTime = 200;')
+
+
+
+for i in range(len(stepArr)):
+    driver.execute_script(stepArr[i] + '(0)')
+    time.sleep(1)
